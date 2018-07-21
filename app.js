cb8bd40376d2933df3e52a845e7e082c6f5d0616
ganghee <idiosauri@naver.com>
2018-07-22 03:51:59 +0900

    채팅 서버 코드

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 /**
  * 채팅하기
  * 
- * 일대일 채팅하기
+<<<<<<< HEAD
+ * 그룹 채팅하기
  * 
- * 'login' 이벤트 처리
- * 'message' 이벤트 처리할 때 대상 소켓 찾아서 전송
+ * 
  *
- * @date 2016-11-10
- * @author Mike
+ * @date 2018-07-22
+=======
+ * Socket.IO를 이용한 일대일 채팅과 그룹 채팅
+ *
+ * @date 2018-07-20
+>>>>>>> parent of e146066... 일대일 대화기능 추가
+ * 
  */
  
 
@@ -49,7 +54,6 @@ var route_loader = require('./routes/route_loader');
 var socketio = require('socket.io');
 
 // cors 사용 - 클라이언트에서 ajax로 요청 시 CORS(다중 서버 접속) 지원
-//하나의 클라이언트에서 여러 서버에 접속하여 다양한 데이터를 가져오기 위해 사용
 var cors = require('cors');
 
 
@@ -163,19 +167,15 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 
 //===== Socket.IO를 이용한 채팅 테스트 부분 =====//
 
-
-// 로그인 아이디 매핑 (로그인 ID -> 소켓 ID)
-var login_ids = {};
-
-
-// socket.io 서버를 시작합니다.
+//socket.io 서버를 시작합니다.
 var io = socketio.listen(server);
 console.log('socket.io 요청을 받아들일 준비가 되었습니다.');
 
-// 클라이언트가 연결했을 때의 이벤트 처리
+//클라이언트가 연결했을 때의 이벤트 처리
 io.sockets.on('connection', function(socket) {
 	console.log('connection info :', socket.request.connection._peername);
 
+<<<<<<< HEAD
     // 소켓 객체에 클라이언트 Host, Port 정보 속성으로 추가
     socket.remoteAddress = socket.request.connection._peername.address;
     socket.remotePort = socket.request.connection._peername.port;
@@ -208,26 +208,118 @@ io.sockets.on('connection', function(socket) {
         	console.dir('나를 포함한 모든 클라이언트에게 message 이벤트를 전송합니다.')
             io.sockets.emit('message', message);
         } else {
-        	// 일대일 채팅 대상에게 메시지 전달
-        	if (login_ids[message.recepient]) {
-        		io.sockets.connected[login_ids[message.recepient]].emit('message', message);
-        		
-        		// 응답 메시지 전송
-                sendResponse(socket, 'message', '200', '메시지를 전송했습니다.');
-        	} else {
-        		// 응답 메시지 전송
-                sendResponse(socket, 'login', '404', '상대방의 로그인 ID를 찾을 수 없습니다.');
-        	}
+            //command 속성으로 일대일 채팅과 그룹 채팅 구별
+            if(message.command == 'chat'){
+        	   // 일대일 채팅 대상에게 메시지 전달
+                if (login_ids[message.recepient]) {
+                    io.sockets.connected[login_ids[message.recepient]].emit('message', message);
+
+                    // 응답 메시지 전송
+                    sendResponse(socket, 'message', '200', '메시지를 전송했습니다.');
+                } else {
+                    // 응답 메시지 전송
+                    sendResponse(socket, 'login', '404', '상대방의 로그인 ID를 찾을 수 없습니다.');
+                }
+            } else if(message.command == 'groupchat'){
+                //방에 들어있는 모든 사용자에게 메시지 전달
+                io.sockets.in(message.recepient).emit('message',message);
+                
+                //응답 메시지 전송
+                sendResponse(socket,'message','200','방['+message.recepient+']의 모든 사용자들에게 메시지를 전달했습니다.');
+            }    
         }
     });
     
+    //room이벤트르 받았을 때의 처리
+    socket.on('room',function(room){
+        console.log('room 이벤트를 받았습니다.');
+        console.dir(room);
+        
+        if(room.command =='create'){
+            if(io.sockets.adapter.rooms[room.roomId]){
+                console.log('방이 이미 만들어져 있습니다.');
+            }else{
+                console.log('방을 새로 만듭니다.');
+                
+                socket.join(room.roomId);
+                
+                var curRoom = io.sockets.adapter.rooms[room.roomId];
+                curRoom.id = room.roomId;
+                curRoom.name = room.roomName;
+                curRoom.owner = room.roomOwner;
+            }
+        } else if (room.command == 'update'){
+            var curRoom = io.sockets.adapter.rooms[room.roomId];
+            curRoom.id = room.roomId;
+            curRoom.name = room.roomName;
+            curRoom.owner = room.roomOwner;
+        } else if (room.command == 'delete'){
+            socket.leave(room.roomId);
+            
+            if(io.sockets.adapter.rooms[room.roomId]){ //방이 만들어져 있는 경우
+                delete io.sockets.adapter.rooms[room.roomId];
+            }else{// 방이 만들어 있지 않은 경우
+                console.log('방이 만들어져 있지 않습니다.')
+            }
+        } else if (room.command == 'join'){//방에 입장하기 요청
+            socket.join(room.roomId);
+            
+            //응답 메시지 전송
+            sendResponse(socket,'room','200','방에 입장했습니다.');
+        } else if (room.command == 'leave'){//방 나가기 요청
+            socket.leave(room.roomId);
+            
+            //응답 메시지 전송
+            sendResponse(socket,'room','200','방에서 나갔습니다.');
+        }
+        
+        var roomList = getRoomList();
+        
+        var output = {command:'list',rooms:roomList};
+        console.log('클라이언트로 보낼 데이터 :' + JSON.stringify(output));
+        
+        io.sockets.emit('room',output);
+    });
+    
+    
+    
+=======
+	// 소켓 객체에 클라이언트 Host, Port 정보 속성으로 추가
+	socket.remoteAddress = socket.request.connection._peername.address;
+	socket.remotePort = socket.request.connection._peername.port;
+>>>>>>> parent of e146066... 일대일 대화기능 추가
 });
 
-
-// 응답 메시지 전송 메소드
-function sendResponse(socket, command, code, message) {
-	var statusObj = {command: command, code: code, message: message};
-	socket.emit('response', statusObj);
+function getRoomList(){
+    console.log('io.sockets.adapter.rooms');
+    
+    var roomList = [];
+    
+    Object.keys(io.sockets.adapter.rooms).forEach(function(roomId){
+        console.log('current room id :' + roomId);
+        var outRoom = io.sockets.adapter.rooms[roomId];
+        
+        //find default room using all attributes
+        var foundDefault = false ;
+        var index = 0;
+        Object.keys(outRoom.sockets).forEach(function(key){
+            console.log('#'+index+':'+key+','+outRoom.sockets[key]);
+            
+            if(roomId ==key){
+                foundDefault = true ;
+                console.log('this is default room');
+            }
+            index++;
+        });
+        
+        if(!foundDefault){
+            roomList.push(outRoom);
+        }
+    });
+    
+    console.log('[Room List]');
+    console.dir(roomList);
+    
+    return roomList;
 }
-
 
